f67c6f20ef8bb5bd9e291b92bd3d63042481f291
aryark <aryark3112@gmail.com>
2023-07-18 09:33:38 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+
+namespace _18July
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exception Hnadling
+            int num1, num2, result;
+            try
+            {
+                Console.WriteLine("Enter First Number");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Second Number");
+                num2 = int.Parse(Console.ReadLine());
+                result = num1 + num2;
+                Console.WriteLine("Result after adding {0} and {1} = \t {2}", num1, num2, result);
+            }
+            catch (Exception ex)
+                {
+                Console.WriteLine("Error!!!"+ex.Message);
+            }
+            finally{
+                Console.WriteLine("End of Program!!!");
+                Console.ReadKey();
+            }
+            
+        }
+    }
+}
